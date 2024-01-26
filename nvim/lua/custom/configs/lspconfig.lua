11eeef6b722f51f63a1c356f7122bddc783654f1
Pouya <liamplm82@gmail.com>
2024-01-26 18:02:47 +0330

    update

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -6,22 +6,22 @@ local lspconfig = require "lspconfig"
 -- if you just want default config for the servers then put them in a table
 -- local servers = { "html", "cssls", "tsserver", "clangd" }
 local servers = {
-  "tsserver",
-  "html",
-  "cssls",
-  "pyright",
-  "angularls",
-  "emmet_ls",
-  "tailwindcss",
-  "docker_compose_language_service",
-  "dockerls",
+    "tsserver",
+    "html",
+    "cssls",
+    "pyright",
+    "angularls",
+    "emmet_ls",
+    "tailwindcss",
+    "docker_compose_language_service",
+    "dockerls",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
 
 -- local utils = require "core.utils"
@@ -38,59 +38,72 @@ end
 -- local servers = { "html", "cssls", "clangd"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
 end
 
 lspconfig.angularls.setup {}
 
 lspconfig.lua_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                    [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
         },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
     },
-  },
 }
 
 local util = require "lspconfig.util"
 local server_config = {
-  filetypes = { "ino", "c", "cpp", "h", "hpp", "objc", "objcpp", "opencl" },
-  root_dir = function(fname)
-    return util.root_pattern ".ccls"(fname)
-      or util.root_pattern "compile_commands.json"(fname)
-      or util.root_pattern(".ccls", "compile_commands.json", "compile_flags.txt")(fname)
-      or util.root_pattern "main.cpp"(fname)
-      or util.find_git_ancestor(fname)
-    --     return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
-    --         or util.find_git_ancestor(fname)
-  end,
-  init_options = {
-    -- compilationDatabaseDirectory = "build";
-    -- compilationDatabaseDirectory = ".pio/build/esp32cam";
-    cache = {
-      directory = "/home/liamplm/.cache/ccls/",
-      -- vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
+    filetypes = { "ino", "c", "cpp", "h", "hpp", "objc", "objcpp", "opencl" },
+    root_dir = function(fname)
+        return util.root_pattern ".ccls"(fname)
+            or util.root_pattern "build/compile_commands.json"(fname)
+            or util.root_pattern "compile_commands.json"(fname)
+            or util.root_pattern(".ccls", "compile_commands.json", "compile_flags.txt")(fname)
+            or util.root_pattern "main.cpp"(fname)
+            or util.find_git_ancestor(fname)
+        --     return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname)
+        --         or util.find_git_ancestor(fname)
+    end,
+    init_options = {
+        compilationDatabaseDirectory = "build",
+        -- compilationDatabaseDirectory = ".pio/build/esp32cam";
+        cache = {
+            directory = "/home/liamplm/.cache/ccls/",
+            -- vim.fs.normalize "~/.cache/ccls" -- if on nvim 0.8 or higher
+        },
+        index = {
+            onChange = true
+        }
     },
-  },
-  on_attach = on_attach,
-  capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 }
-require("ccls").setup { lsp = { lspconfig = server_config } }
+require("ccls").setup {
+    lsp = {
+        lspconfig = server_config,
+
+        codelens = {
+            enable = true,
+            events = { "BufWritePost", "InsertLeave" },
+        },
+    },
+}
 
 --
 -- lspconfig.pyright.setup { blabla}
--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -4,19 +4,37 @@ local b = null_ls.builtins
 
 local sources = {
 
-  -- webdev stuff
-  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-    
-  b.formatting.prettierd.with { filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+    -- webdev stuff
+    -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
 
-  -- Lua
-  b.formatting.stylua,
+    b.formatting.prettierd.with {
+        filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "css",
+            "scss",
+            "html",
+            "markdown",
+            "css",
+        },
+    }, -- so prettier works only on these filetypes
 
-  -- cpp
-  b.formatting.clang_format,
+    -- Lua
+    b.formatting.stylua,
+
+    -- cpp
+    b.formatting.clang_format,
+    -- offsetEncoding = { "utf-8", "utf-16" },
 }
 
 null_ls.setup {
-  debug = true,
-  sources = sources,
+    debug = true,
+    sources = sources,
+    on_init = function(new_client, _)
+        new_client.offset_encoding = "utf-32"
+    end,
+    -- offsetEncoding = { "utf-8", "utf-16" },
 }

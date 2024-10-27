6b0b4929f9d7a9e4cdf9bf0225e2243a24707a03
Pouya <liamplm82@gmail.com>
2024-10-28 01:04:51 +0330

    support text cases add plugins: nvim spider, text-case

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -4,6 +4,47 @@ local overrides = require "custom.configs.overrides"
 local plugins = {
     -- Override plugin definition options
     {
+        "johmsalas/text-case.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function()
+            require("textcase").setup {}
+            require("telescope").load_extension "textcase"
+        end,
+        keys = {
+            "ga", -- Default invocation prefix
+            { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+        },
+        cmd = {
+            -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+            "Subs",
+            "TextCaseOpenTelescope",
+            "TextCaseOpenTelescopeQuickChange",
+            "TextCaseOpenTelescopeLSPChange",
+            "TextCaseStartReplacingCommand",
+        },
+        -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+        -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+        -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+        lazy = true,
+    },
+    {
+        "chrisgrieser/nvim-spider",
+        lazy = true,
+        keys = {
+            {
+                "e",
+                "<cmd>lua require('spider').motion('e')<CR>",
+                mode = { "n", "o", "x" },
+            },
+            {
+                "w",
+                "<cmd>lua require('spider').motion('w')<CR>",
+                mode = { "n", "o", "x" },
+            },
+            -- ...
+        },
+    },
+    {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             { "nvim-treesitter/nvim-treesitter" },
